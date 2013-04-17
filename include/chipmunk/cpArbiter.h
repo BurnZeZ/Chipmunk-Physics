304036e8a4af2d9b34859d30a35069bf0c9e0164
BurnZeZ <BurnZeZ@live.com>
2013-04-17 01:09:32 -0400

    Revert "Making macro arguments safer"
    This reverts commit 820e05f6851e2979cf4d5c4b3dede56d428ad866.

--- include/chipmunk/cpArbiter.h
+++ include/chipmunk/cpArbiter.h
@@ -156,7 +156,7 @@ static inline void cpArbiterGetShapes(const cpArbiter *arb, cpShape **a, cpShape
 	}
 }
 /// A macro shortcut for defining and retrieving the shapes from an arbiter.
-#define CP_ARBITER_GET_SHAPES(__arb__, __a__, __b__) cpShape *__a__, *__b__; cpArbiterGetShapes(__arb__, &__a__, &__b__);
+#define CP_ARBITER_GET_SHAPES(arb, a, b) cpShape *a, *b; cpArbiterGetShapes(arb, &a, &b);
 
 /// Return the colliding bodies involved for this arbiter.
 /// The order of the cpSpace.collision_type the bodies are associated with values will match
@@ -168,7 +168,7 @@ static inline void cpArbiterGetBodies(const cpArbiter *arb, cpBody **a, cpBody *
 	(*b) = shape_b->body;
 }
 /// A macro shortcut for defining and retrieving the bodies from an arbiter.
-#define CP_ARBITER_GET_BODIES(__arb__, __a__, __b__) cpBody *__a__, *__b__; cpArbiterGetBodies(__arb__, &__a__, &__b__);
+#define CP_ARBITER_GET_BODIES(arb, a, b) cpBody *a, *b; cpArbiterGetBodies(arb, &a, &b);
 
 /// A struct that wraps up the important collision data for an arbiter.
 typedef struct cpContactPointSet {
